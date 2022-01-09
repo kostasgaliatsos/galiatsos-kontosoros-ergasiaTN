290073a2a0e6267bd2ab052deff927da0d1906ee
None <kostas.gal1@gmail.com>
2022-01-10 01:06:20 +0200

    added remote service

--- src/app/results.service.ts
+++ src/app/results.service.ts
@@ -10,7 +10,7 @@ export class ResultsService {
   callService(item: any) {
     const formData = new FormData();
     formData.append('image', item);
-    return this.http.post('http://127.0.0.1:5000/results',formData)
+    return this.http.post('http://galiatsoskontosorostn.eu.pythonanywhere.com/results',formData)
     
   }
 }
